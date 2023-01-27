5bb5a4ca6d787f94a1dbb5e0751a2e23658f2089
dazkool <nvkulkarni.1108@gmail.com>
2023-01-27 23:48:10 +0530

    day 19

---
+++ react0/links.md
@@ -0,0 +1,1 @@
+[Adding svg icons to react](https://rb.gy/w9qik5)
--- react0/src/components/Navbar.jsx
+++ react0/src/components/Navbar.jsx
@@ -1,7 +1,16 @@
+import Globe from "../assets/globe.jsx";
 export default function Navbar() {
   return (
-    <nav>
-      <div className="logo"></div>
+    <nav className="flex navbar">
+      {/* <div>
+        <Globe className="logo" />
+      </div> */}
+      <ul className="navbar-content ff-sans-normal fs-500 letter-spacing-3">
+        <li>home</li>
+        <li>about</li>
+        <li>team</li>
+        <li>contact</li>
+      </ul>
     </nav>
   );
 }
